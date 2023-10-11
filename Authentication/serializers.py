26bb4f822b7f493a81bc702e20c54dab936c6f3b
Salma <samelmahdy077@gmail.com>
2023-10-11 12:47:07 +0300

    initial setups and authentication module
    - add initial setup to use jwt and swagger and DRF in the project
    - add authentication module to register user, login and verify email with token

---
+++ Authentication/serializers.py
@@ -0,0 +1,63 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email=serializers.EmailField(max_length=255,min_length=3)
+    password=serializers.CharField(max_length=68, min_length=6, write_only=True)
+    
+    class Meta:
+        model=User
+        fields=['email','username','password']
+        
+    def validate(self,attrs):
+        email=attrs.get('email','')
+        username=attrs.get('username','')
+        
+        if not username.isalnum():
+            raise serializers.ValidationError('The username should only contain alpanumeric characters')
+        return attrs
+    
+    def create(self, validated_data):
+        password=validated_data.pop("password")
+        user=super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    
+
+class EmailVerificationSerializer(serializers.ModelSerializer):
+    token=serializers.CharField(max_length=555)
+    class Meta:
+        model=User
+        fields=['token']
+        
+
+class LoginSerializer(serializers.ModelSerializer):
+    email=serializers.EmailField(max_length=255,min_length=3)
+    password=serializers.CharField(max_length=68,min_length=6,write_only=True)
+    username=serializers.CharField(max_length=68,min_length=6,read_only=True)
+    tokens=serializers.CharField(read_only=True)
+    
+    class Meta:
+        model=User
+        fields=['email','password','username','tokens']
+        
+    def validate(self, attrs):
+        email=attrs.get('email','')
+        password=attrs.get('password','')
+        user=auth.authenticate(email=email,password=password)
+        if not user:
+            raise AuthenticationFailed('Inalid Credentials, try again')
+        if not user.is_active:
+            raise AuthenticationFailed('Account disabled, contact admin')
+        if not user.is_verified:
+            raise AuthenticationFailed('Email is not verified')
+        
+        return {
+            'email':user.email,
+            'username':user.username,
+            'tokens':user.tokens()
+        }
---
+++ Authentication/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from django.conf import settings
+from rest_framework import generics,status, views
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework_simplejwt.tokens import RefreshToken
+from jwt import decode, ExpiredSignatureError, DecodeError
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from .serializers import RegisterSerializer,EmailVerificationSerializer,LoginSerializer
+from .models import User
+from .utils import Util
+
+
+class RegisterView(generics.GenericAPIView):
+    
+    serializer_class =RegisterSerializer
+    permission_classes=[]
+    
+    def post(self,request:Request):
+        user = request.data
+        serializer= self.serializer_class(data=user)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        
+        user_data=serializer.data
+        
+        
+        user= User.objects.get(email=user_data['email'])
+        access_token=RefreshToken.for_user(user).access_token
+        # TODO:: send Email to verify user
+        # access_token= RefreshToken.for_user(user).access_token
+        # current_site_domain=get_current_site(request).domain
+        # relativeLink=reverse('email-verify')
+        
+        # absurl='http://'+current_site_domain+relativeLink+"?token="+str(access_token)
+        
+        # email_body= 'Hi'+user.username+'use below link to verify your email \n'+absurl
+        # data={
+        #     "email_subject":'Verify your email',
+        #     "email_body":email_body,
+        #     "to_email":user.email
+        # }
+        # Util.send_email(data)
+        data={
+            'user_data':user_data,
+            'access_token':str(access_token)
+        }
+        return Response(data,status=status.HTTP_201_CREATED)
+        
+class VerifyEmail(views.APIView):
+    serializer_class=EmailVerificationSerializer
+    
+    token_param_config=openapi.Parameter('token',in_=openapi.IN_QUERY,description='Enter user acces Token',type=openapi.TYPE_STRING)
+    @swagger_auto_schema(manual_parameters=[token_param_config])
+    def get(self,request):
+        token = request.GET.get('token')
+        if not token:
+            return Response({'error': 'Token not provided'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            print(settings.SECRET_KEY)
+            payload= decode(token,settings.SECRET_KEY,algorithms=['HS256'])
+            print('payload',payload)
+            user_id = payload.get('user_id')
+            if not user_id:
+                return Response({'error': 'Invalid token payload'}, status=status.HTTP_400_BAD_REQUEST)
+
+            user = User.objects.get(id=user_id)
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
+
+        except ExpiredSignatureError:
+            return Response({'error': 'Activation link expired'}, status=status.HTTP_400_BAD_REQUEST)
+
+        except DecodeError:
+            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class=LoginSerializer
+    def post(self,request):
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            return Response(serializer.data,status=status.HTTP_200_OK)
